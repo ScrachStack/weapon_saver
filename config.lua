279ed16739a5236e826e2f9cc5ba50735192c14b
ScratchStack <122732007+ScrachStack@users.noreply.github.com>
2025-11-22 16:31:01 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,20 @@
+Config = {
+    Storage = "json",
 
+    Identifier = "steam",
+
+    Json = {
+        File = "data.json",
+        AutoCreate = true
+    },
+
+    MySQL = {
+        AutoInsert = false,
+    },
+
+    SaveTimer = {
+        SaveEvery = 30
+    },
+
+    Debug = false
+}
