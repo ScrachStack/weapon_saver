6238cf90eb6412d67dcebde45a28207d7c1d6eca
ScratchStack <122732007+ScrachStack@users.noreply.github.com>
2025-11-22 16:29:51 -0600

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,1 +1,37 @@
+CreateThread(function()
+    Wait(1500)
+    TriggerServerEvent("weapons:requestLoad")
+end)
 
+RegisterNetEvent("weapons:giveBack")
+AddEventHandler("weapons:giveBack", function(weapons)
+    local ped = PlayerPedId()
+
+    for _, w in ipairs(weapons) do
+        GiveWeaponToPed(ped, tonumber(w.hash), w.ammo or 250, false, true)
+    end
+end)
+
+local function GetAllWeaponsDynamic()
+    local ped = PlayerPedId()
+    local weapons = {}
+
+    for hash = 0, 0xFFFFFFFF do
+        local group = GetWeapontypeGroup(hash)
+        if group and group ~= 0 and HasPedGotWeapon(ped, hash, false) then
+            table.insert(weapons, {
+                hash = hash,
+                ammo = GetAmmoInPedWeapon(ped, hash)
+            })
+        end
+    end
+
+    return weapons
+end
+
+
+AddEventHandler("onResourceStop", function(res)
+    if res == GetCurrentResourceName() then
+        TriggerServerEvent("weapons:save", GetAllWeaponsDynamic())
+    end
+end)
